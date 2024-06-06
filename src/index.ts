45e223fc990da1f6b16e19be7f373fbb0b3ccaf4
Zodomo <zodomo@proton.me>
2024-06-06 13:49:33 -0500

    switched to character check to checking banned characters array (#9)

--- src/index.ts
+++ src/index.ts
@@ -137,10 +137,11 @@ export const normalizeName = (name: string): string => {
 };
 
 export const isNameValid = (name: string): boolean => {
+  const bannedCharacters = ['/', ' '];
   const normalized = normalizeName(name);
+  // Check if name contains any banned characters
+  if (bannedCharacters.some(char => normalized.includes(char))) return false;
   // Is name > 32 bytes
   if (encodeURI(normalized).split(/%..|./).length - 1 > 32) return false;
-  // Does name contain '/'
-  if (normalized.includes('/')) return false;
   return true;
 };

293706b2d30104fb08278d3e3580f2556c55c6ae
rye <99738501+ryespresso@users.noreply.github.com>
2024-06-14 14:39:23 -0400

    Add Blast to network type

--- src/types.ts
+++ src/types.ts
@@ -13,7 +13,18 @@ export type Cluster = {
   wallets: Wallet[];
 };
 
-export type Network = '1' | '10' | '56' | '137' | '8453' | '17000' | '42161' | '43114' | '11155111' | 'solana';
+export type Network =
+  | '1'
+  | '10'
+  | '56'
+  | '137'
+  | '8453'
+  | '81457'
+  | '17000'
+  | '42161'
+  | '43114'
+  | '11155111'
+  | 'solana';
 
 export type NameAvailability = {
   name: string;
